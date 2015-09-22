1c707741f8dd06f92bc2304da4fae94253e00753
Dodzidenu Y. Dzakuma <ddzakuma@kmsnet.com>
2015-09-22 12:08:58 -0500

    More stuff.

--- RubeGoldbergProgram/RubeGoldbergProgram/RubeGoldbergProgram.cs
+++ RubeGoldbergProgram/RubeGoldbergProgram/RubeGoldbergProgram.cs
@@ -12,8 +12,11 @@ namespace RubeGoldbergProgram
 		{
 			Console.WriteLine("This program does something simple in the most complicated way possible.");
 
-			int randomNumber = GetRandomNumber();
-			Console.WriteLine("Let's start with the random number. [{0}]", randomNumber);
+			int randomNumberOne = GetRandomNumber();
+			int randomNumberTwo = GetRandomNumber();
+			Console.WriteLine("Let's start with the random number. [{0}]", randomNumberOne);
+
+			Console.WriteLine("And let's use the random number. [{0}]", randomNumberTwo);
 
 			Console.WriteLine("Press any key to continue.");
 			Console.ReadLine();
